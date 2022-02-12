419d814c8d1d2ac868d0967052e3dc9c35f6094b
mounikabodige <mounikabodige1994@gmail.com>
2022-02-12 18:53:27 -0500

    Added Progress bar.

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText  = document.getElementById('questionCounter');
+const progressText  = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 let acceptingAnswers = false;
 let availibleQuestions = [];
@@ -50,10 +50,17 @@ startGame = () => {
 
 getNewQuestion = () => {
     if(availibleQuestions.length == 0 || questionCounter > MAX_QUESTIONS){
+        //go to the end page.
         return window.location.assign("end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+    //Update the progress bar.
+
+    progressBarFull.style.width = `${(questionCounter/ MAX_QUESTIONS) * 100}%`;
+
+
     const questionIndex = Math.floor(Math.random() * availibleQuestions.length);
     currQuestion = availibleQuestions[questionIndex];
     question.innerText = currQuestion.question;
