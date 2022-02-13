a23bdf173419a733823ddf7319e941477adc9bc8
mounikabodige <mounikabodige1994@gmail.com>
2022-02-12 19:53:32 -0500

    Saving the data of last 5 players.

--- end.js
+++ end.js
@@ -4,6 +4,9 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+
 username.addEventListener( 'keyup', () =>{
     saveScore.disabled = !username.value;
 });
@@ -11,4 +14,16 @@ username.addEventListener( 'keyup', () =>{
 saveHighScore = e => {
     console.log("Clicked the save button");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score );
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem('higHScores', JSON.stringify(highScores));
+    window.location.assign("/");
 };
